f14dbf37357eca665eb92a62b988ab9c38803a67
zaitsev69 <126803254+zaitsev69@users.noreply.github.com>
2024-04-16 10:41:33 +0200

    Implémentation Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IncidentController;
+
+Route::post('/incident', [IncidentController::class, 'store'])->name('incident.store');
 
 Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-use App\Http\Controllers\IncidentController;
-Route::post('/incident', [IncidentController::class, 'store'])->name('incident.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
